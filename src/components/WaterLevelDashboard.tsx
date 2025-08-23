001dd50e15bdf8fa991cf6098072415347bf4c65
Ogidol <ogundele115@gmail.com>
2025-08-23 09:58:57 +0100

    first commit

--- src/components/WaterLevelDashboard.tsx
+++ src/components/WaterLevelDashboard.tsx
@@ -475,7 +475,7 @@ export default function WaterLevelDashboard({
         <div className="text-center">
           <div
             className={`${
-              is320 ? "w-12 h-12" : "w-16 h-16"
+              is320 ? "w-12 h-screen" : "w-16 h-16"
             } border-4 border-blue-200 border-t-blue-600 rounded-full animate-spin mx-auto mb-4`}
           ></div>
           <h2
